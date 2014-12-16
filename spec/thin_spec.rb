be28f395b9d315badd3d6c698778e9fd00acb0fa
Andreas Altendorfer <andreas.altendorfer@runtastic.com>
2014-12-16 18:00:10 +0100

    AnyThing expected to be able to rot

--- spec/thin_spec.rb
+++ spec/thin_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 require 'the_thing'
 
 describe TheThing do
+
   it "is a thing" do
     expect(TheThing).to be_awesome
   end
+
+  it "can rot" do
+    TheThing.rot!
+    expect(TheThing).not_to be_awesome
+  end
+
 end
