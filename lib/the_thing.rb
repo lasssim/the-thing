eb4c81609df996a187b9ca35ff8ab0e140b4bb2b
maf <maf@runtastic.com>
2014-12-22 08:35:28 +0100

    Make tests green

--- lib/the_thing.rb
+++ lib/the_thing.rb
@@ -1,7 +1,11 @@
 class TheThing
+  
+  attr_accessor :sent_messages, :received_messages
 
   def initialize
     @awesome = true
+    @sent_messages     = []
+    @received_messages = []
   end
 
   def awesome?
@@ -10,5 +14,14 @@ class TheThing
 
   def rot!
     @awesome = false
+  end
+
+  def receive(message)
+    @received_messages << message
+  end
+
+  def send_message(message, receiver)
+    receiver.receive(message)
+    @sent_messages << message
   end
 end
